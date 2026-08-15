d66d9c200c2f11b30cec87cc94438265834211fe
Fareeha Asif <fareehaasif@Fareehas-MacBook-Pro.local>
2026-08-14 20:53:53 -0400

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
